a36e96191f0fe7129d64fda9147c7ab8a50b7c57
Kabra, Chandraprakash <Chandraprakash.Kabra@spglobal.com>
2020-03-15 12:22:21 +0530

    This is whole code structure

--- EFNgApp/ClientApp/src/app/app.module.ts
+++ EFNgApp/ClientApp/src/app/app.module.ts
@@ -1,34 +1,78 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-
+import {
+  NgModule
+} from '@angular/core';
+import {
+  EmpserviceService
+} from './services/empservice.service'
+import {
+  CommonModule
+} from '@angular/common';
+import {
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
+import {
+  HttpModule
+} from '@angular/http';
+import {
+  RouterModule
+} from '@angular/router';
+import {
+  AddEmployeeComponent
+} from './components/add-employee/add-employee.component';
+import {
+  FetchEmployeeComponent
+} from './components/fetch-employee/fetch-employee.component';
+import {
+  HomeComponent
+} from './home/home.component';
+import {
+  NavMenuComponent
+} from './nav-menu/nav-menu.component';
+import {
+  AppComponent
+} from './app.component';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    AddEmployeeComponent,
+    FetchEmployeeComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
+    BrowserModule.withServerTransition({
+      appId: 'ng-cli-universal'
+    }),
+    CommonModule,
+    HttpModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ReactiveFormsModule,
+    RouterModule.forRoot([{
+      path: '',
+      component: HomeComponent,
+      pathMatch: 'full'
+    }, {
+      path: 'home',
+      component: HomeComponent
+    }, {
+      path: 'fetch-employee',
+      component: FetchEmployeeComponent
+    }, {
+      path: 'add-employee',
+      component: AddEmployeeComponent
+    }, {
+      path: 'employee/edit/:id',
+      component: AddEmployeeComponent
+    }, {
+      path: '**',
+      redirectTo: 'home'
+    }])
   ],
-  providers: [],
+  providers: [EmpserviceService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
---
+++ EFNgApp/Models/Angular6_core2Context.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EFNgApp.Models
+{
+    public partial class Angular6_core2Context : DbContext
+    {
+        public Angular6_core2Context()
+        {
+        }
+
+        public Angular6_core2Context(DbContextOptions<Angular6_core2Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblCities> TblCities { get; set; }
+        public virtual DbSet<TblEmployee> TblEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=II31-HVSW5Q2\\SQLEXPRESS;Initial Catalog=Angular6_core2;User ID=sa;Password=sql@2019;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblCities>(entity =>
+            {
+                entity.HasKey(e => e.CityId);
+
+                entity.ToTable("tblCities");
+
+                entity.Property(e => e.CityId).HasColumnName("CityID");
+
+                entity.Property(e => e.CityName)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblEmployee>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId);
+
+                entity.ToTable("tblEmployee");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Department)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasMaxLength(6)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
